a0426a12493f9548e7fd3575574f94451d4795a2
manne ohrstrom <manne@shotgunsoftware.com>
2014-04-08 11:08:02 +0100

    Interface tweaks to the Simple Shotgun Model Wrapper.

--- python/shotgun_model/simpleshotgunmodel.py
+++ python/shotgun_model/simpleshotgunmodel.py
@@ -8,35 +8,51 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import tank
+import sgtk
 from .shotgunmodel import ShotgunModel
 
-from tank.platform.qt import QtCore, QtGui
+from sgtk.platform.qt import QtCore, QtGui
 
 class SimpleShotgunModel(ShotgunModel):
     """
-    Convenience wrapper around the Shotgun model for quick and easy access
+    Convenience wrapper around the Shotgun model for quick and easy access.
+    
+    When you quickly want to display some shotgun data in a QT view of some kind,
+    this class may come in handy. Simply instantiate it and call load_data whenever
+    you want to load up a new shotgun query into your view.
+    
+    It derives from ShotgunModel so all the customization methods available in the
+    normal ShotgunModel can also be subclassed from this class.
     """
  
     def __init__(self, parent):
         """
         Constructor.
         
-        The simple shotgun model will put the load spinner on top of the specified parent
+        The simple shotgun model will put the load spinner on top of the specified parent.
+        
+        :param parent: QWidget which this model will be parented under. This widget will
+                       also be used to paint a spinner and display error messages.
         """
         ShotgunModel.__init__(self, parent, download_thumbs=True)
         self.set_overlay_parent(parent)
         
-    def load_data(self, entity_type, fields):
+    def load_data(self, entity_type, filters=None, fields=None):
         """
         Loads shotgun data into the model, using the cache if possible.
-        """
-        ShotgunModel._load_data(self, 
-                               entity_type=entity_type, 
-                               filters=filters, 
-                               hierarchy=["code"],
-                               fields=fields,
-                               order=[])
+        The model is not nested and the first field that is specified
+        via the fields parameter (code by default) will be used as the default
+        name for all model items. 
         
+        :param entity_type: Shotgun Entity Type to load data for
+        :param filters: Shotgun API find-style filter list. If no list is specified, all records
+                        for the given entity type will be retrieved.
+        :param fields: List of Shotgun fields to retrieve. If not spefified, the 'code' field
+                       will be retrieved.
+        """
+        filters = filters or []
+        fields = fields or ["code"]
+        hierarchy = fields[0]
+        ShotgunModel._load_data(self, entity_type, filters, hierarchy, fields)
         self._refresh_data()
         
