dde5366c01475c6942750719df7f0629438da6a6
Stéphane Deverly <stephane.deverly@shotgunsoftware.com>
2018-02-02 09:35:01 +0100

    For #39289, deadlock fix. (#79)
    * Removed all logging from background thread.
    * Only kept the bare minimum in the code run on the background thread, just in case.

--- framework.py
+++ framework.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import sgtk
@@ -17,7 +17,7 @@ import os
 import time
 
 class ShotgunUtilsFramework(sgtk.platform.Framework):
-    
+
     # List of top folders in the cache which should be considered for old
     # data clean up.
     _CLEANUP_FOLDERS = [
@@ -31,7 +31,7 @@ class ShotgunUtilsFramework(sgtk.platform.Framework):
 
     ##########################################################################################
     # init and destroy
-            
+
     def init_framework(self):
         """
         Init this framework.
@@ -42,7 +42,7 @@ class ShotgunUtilsFramework(sgtk.platform.Framework):
         self._stop_cleanup = False
         self._bg_cleanup_thread = None
         self._post_old_data_cleanup()
-    
+
     def destroy_framework(self):
         """
         Destroy this framework.
@@ -59,30 +59,10 @@ class ShotgunUtilsFramework(sgtk.platform.Framework):
                 # If the clean up is not completed yet, log why we are waiting.
                 self.log_info("Waiting for old data clean up to complete...")
             self._bg_cleanup_thread.join()
-    
+
     def _post_old_data_cleanup(self):
         """
         Post a cleanup of old cached data in the background.
-        """
-        try:
-            self.log_debug(
-                "Posting old cached data clean up..."
-            )
-            self._stop_cleanup = False
-            # Qt might not be yet available at this stage (e.g. in tk-desktop),
-            # so we can't use a background task manager or a QThread, use instead
-            # regular Python Thread to post the clean up in the background.
-            self._bg_cleanup_thread = threading.Thread(
-                target=self._remove_old_cached_data,
-                name="%s Clean Up" % self.name
-            )
-            self._bg_cleanup_thread.start()
-        except Exception as e:
-            self.log_warning("Unable to post data clean up: %s" % e)
-
-    def _remove_old_cached_data(self):
-        """
-        Remove old data files cached by this bundle.
 
         A file is considered old if it was not modified in the last number of days
         specified by the `_CLEANUP_GRACE_PERIOD` class member value, which must
@@ -97,16 +77,54 @@ class ShotgunUtilsFramework(sgtk.platform.Framework):
         The list of top folders to consider for clean up is explicitly defined in
         the `_CLEANUP_FOLDERS` class member list, anything outside of those will
         never be removed by the clean up.
-
-        :raises: ValueError if the grace period is smaller than 1.
         """
-        grace_period = self._CLEANUP_GRACE_PERIOD
+        try:
+            self.log_debug(
+                "Posting old cached data clean up..."
+            )
+            self._stop_cleanup = False
+
+            grace_period = self._CLEANUP_GRACE_PERIOD
+            delta = datetime.timedelta(days=grace_period)
+            now = datetime.datetime.now()
+
+            # Clean up the site cache and the project cache locations, only consider
+            # folders specified in _CLEANUP_FOLDERS
+            cache_locations = [
+                os.path.join(self.site_cache_location, folder) for folder in self._CLEANUP_FOLDERS
+            ]
+            cache_locations.extend([
+                os.path.join(self.cache_location, folder) for folder in self._CLEANUP_FOLDERS
+            ])
+            self.logger.debug(
+                "Cleaning all files with a modification date older than %s under locations "
+                "%s" % ((now - delta), ", ".join(cache_locations))
+            )
+            # Qt might not be yet available at this stage (e.g. in tk-desktop),
+            # so we can't use a background task manager or a QThread, use instead
+            # regular Python Thread to post the clean up in the background.
+            self._bg_cleanup_thread = threading.Thread(
+                target=self._remove_old_cached_data,
+                args=[grace_period] + cache_locations,
+                name="%s Clean Up" % self.name
+            )
+            self._bg_cleanup_thread.start()
+        except Exception as e:
+            self.log_warning("Unable to post data clean up: %s" % e)
+
+    def _remove_old_cached_data(self, grace_period, *cache_locations):
+        """
+        Remove old data files cached by this bundle in the given cache locations.
+
+        :param int grace_period: Time period, in days, a file without
+                                     modification should be kept around.
+        :param cache_locations: A list of cache locations to clean up.
+        :raises: ValueError if the grace period is smaller than one day.
+        """
         if grace_period < 1:
             raise ValueError(
                 "Invalid grace period value %d, it must be a least 1" % grace_period
             )
-        now_timestamp = time.time()
-        now = datetime.datetime.now()
         delta = datetime.timedelta(days=grace_period)
         # Datetime total_seconds was introduced in Python 2.7, so compute the
         # value ourself.
@@ -114,18 +132,9 @@ class ShotgunUtilsFramework(sgtk.platform.Framework):
             delta.microseconds + (delta.seconds + delta.days * 24 * 3600) * 10**6
         ) / 10**6
 
-        # Clean up the site cache and the project cache locations, only consider
-        # folders specified in _CLEANUP_FOLDERS
-        cache_locations = [
-            os.path.join(self.site_cache_location, folder) for folder in self._CLEANUP_FOLDERS
-        ]
-        cache_locations.extend([
-            os.path.join(self.cache_location, folder) for folder in self._CLEANUP_FOLDERS
-        ])
-        self.logger.debug(
-            "Cleaning all files with a modification date older than %s under locations "
-            "%s" % ((now - delta), ", ".join(cache_locations))
-        )
+        # Please note that we can't log any message from this background thread
+        # without the risk of causing deadlocks.
+        now_timestamp = time.time()
         # Check if we should stop and bail out immediately if so.
         if self._stop_cleanup:
             return
@@ -143,13 +152,8 @@ class ShotgunUtilsFramework(sgtk.platform.Framework):
                         if now_timestamp - file_stats.st_mtime > grace_in_seconds:
                             filesystem.safe_delete_file(file_path)
                     except Exception as e:
-                        # Log the error for debug purpose
-                        self.logger.debug(
-                            "Warning: couldn't check %s for removal: %s" % (
-                                file_path, e
-                            ),
-                            exc_info=True,
-                        )
+                        # Silently ignore the error
+                        pass
 
                 for name in dirs:
                     # Check if we should stop and bail out immediately if so.
@@ -161,15 +165,6 @@ class ShotgunUtilsFramework(sgtk.platform.Framework):
                         if not os.listdir(dir_path):
                             filesystem.safe_delete_folder(dir_path)
                     except Exception as e:
-                        # Log the error for debug purpose
-                        self.logger.debug(
-                            "Warning: couldn't check %s for removal: %s" % (
-                                dir_path, e
-                            ),
-                            exc_info=True,
-                        )
-                        # And ignore it
+                        # Silently ignore the error
                         pass
-        self.logger.debug(
-            "Old data cleanup completed in %s" % (datetime.datetime.now() - now)
-        )
+
