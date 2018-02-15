a5aa7d3f6dac413d988c5a0718f065c65be102ab
Manne Öhrström <manne.ohrstrom@autodesk.com>
2018-02-15 16:29:57 +0000

    docstrings and polish

--- python/external_config/configuration_state.py
+++ python/external_config/configuration_state.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Shotgun Software Inc.
+# Copyright (c) 2018 Shotgun Software Inc.
 #
 # CONFIDENTIAL AND PROPRIETARY
 #
@@ -25,15 +25,25 @@ class ConfigurationState(QtCore.QObject):
 
     Looks at software entities and Pipeline Configurations.
 
-    Calling refresh will check for changes and if changes have
-    happened, the state_changed signal will be emitted.
+    As an example, changing a software entity may affect
+    the list of registered commands.
+
+    Signal Interface
+    ----------------
+
+    :signal state_changed(): Indicates that the state has changed since
+        it was last checked.
+
     """
     state_changed = QtCore.Signal()
 
-    def __init__(self, bg_task_manager, parent=None):
+    def __init__(self, bg_task_manager, parent):
         """
-        @param bg_task_manager:
-        @param parent:
+        :param bg_task_manager: Background task manager to use for any asynchronous work. If
+            this is None then a task manager will be created as needed.
+        :type bg_task_manager: :class:`~task_manager.BackgroundTaskManager`
+        :param parent: The model's parent.
+        :type parent: :class:`~PySide.QtGui.QObject`
         """
         super(ConfigurationState, self).__init__(parent)
         self._software_model = SoftwareModel(bg_task_manager, parent)
@@ -46,28 +56,35 @@ class ConfigurationState(QtCore.QObject):
         """
         Trigger an asynchronous background check of the Shotgun site
         configuration state. If a change is detected, indicating that
-        configurations should be recomputed, a state_changed signal is emitted.
+        configurations should be recomputed, a ``state_changed`` signal is emitted.
         """
         self._pipeline_config_model.load()
         self._software_model.load()
 
     def shut_down(self):
         """
-        turn stuff off
+        Shut down and deallocate.
         """
         self._software_model.destroy()
         self._pipeline_config_model.destroy()
 
     def get_hash(self):
         """
-        Returns a hash representing the global state of Shotgun or None if not yet defined.
+        Returns a hash representing the global state of Shotgun.
+
+        :returns: Hash string or ``None`` if not yet defined.
         """
-        return str(self._software_model.get_hash()) + str(self._pipeline_config_model.get_hash())
+        sw_hash = self._software_model.get_hash()
+        pc_hash = self._pipeline_config_model.get_hash()
+        if sw_hash is None or pc_hash is None:
+            return None
+        return str(sw_hash) + str(pc_hash)
 
     def _on_software_refreshed(self, has_changed):
         """
-        Software entity model is updated
-        :param has_changed:
+        Software entity data has been retrieved
+
+        :param bool has_changed: The cached data changed
         """
         if has_changed:
             logger.debug("Shotgun software entity change detected.")
@@ -75,8 +92,9 @@ class ConfigurationState(QtCore.QObject):
 
     def _on_pipeline_configs_refreshed(self, has_changed):
         """
-        Software entity model is updated
-        :param has_changed:
+        Pipeline Config entity data has been retrieved
+
+        :param bool has_changed: The cached data changed
         """
         if has_changed:
             logger.debug("Shotgun pipeline config change detected.")
@@ -87,25 +105,40 @@ class SoftwareModel(ShotgunModel):
     """
     All software entities
     """
-
     def __init__(self, bg_task_manager, parent):
         """
         :param parent: QT parent object
         """
-        super(SoftwareModel, self).__init__(parent, download_thumbs=False, bg_task_manager=bg_task_manager)
+        super(SoftwareModel, self).__init__(
+            parent,
+            download_thumbs=False,
+            bg_task_manager=bg_task_manager
+        )
 
     def load(self):
         """
-        Load all pipeline configuration data into model.
+        Load all data into model.
         """
         hierarchy = ["id"]
         fields = ["code", "updated_at"]
         self._load_data("Software", [], hierarchy, fields)
         self._refresh_data()
 
-    def get_sg_data(self):
+    def get_hash(self):
         """
-        Access shotgun data.
+        Computes a hash representing the state of all entities.
+
+        :returns: Hash int or None if nothing is loaded.
+        """
+        sg_data = self._get_sg_data()
+        if sg_data is None:
+            return None
+        else:
+            return hash(str(sg_data))
+
+    def _get_sg_data(self):
+        """
+        Currently loaded Shotgun data.
 
         :returns: The sg data dictionary for the associated item,
                   None if not available.
@@ -117,43 +150,53 @@ class SoftwareModel(ShotgunModel):
             for idx in range(self.rowCount()):
                 item = self.item(idx)
                 data.append(item.get_sg_data())
-        return data
 
-    def get_hash(self):
-        """
-        Returns a hash representing the state of all sw entities
-        or None if nothing is loaded
-        """
-        sg_data = self.get_sg_data()
-        if sg_data is None:
-            return None
-        else:
-            return hash(str(sg_data))
+        return data
 
 
 class PipelineConfigModel(ShotgunModel):
     """
-    All Pipeline Configurations
+    Pipeline configurations for all active projects.
     """
-
     def __init__(self, bg_task_manager, parent):
         """
         :param parent: QT parent object
         """
-        super(PipelineConfigModel, self).__init__(parent, download_thumbs=False, bg_task_manager=bg_task_manager)
+        super(PipelineConfigModel, self).__init__(
+            parent,
+            download_thumbs=False,
+            bg_task_manager=bg_task_manager
+        )
 
     def load(self):
         """
-        Load all pipeline configuration data into model.
+        Load all data into model.
         """
         hierarchy = ["id"]
         fields = ["code", "updated_at"]
-        self._load_data("PipelineConfiguration", [], hierarchy, fields)
+        self._load_data(
+            "PipelineConfiguration",
+            [["project.Project.archived", "is", False]],
+            hierarchy,
+            fields
+        )
         self._refresh_data()
 
-    def get_sg_data(self):
+    def get_hash(self):
         """
-        Access current shotgun data.
+        Computes a hash representing the state of all entities.
+
+        :returns: Hash int or None if nothing is loaded.
+        """
+        sg_data = self._get_sg_data()
+        if sg_data is None:
+            return None
+        else:
+            return hash(str(sg_data))
+
+    def _get_sg_data(self):
+        """
+        Currently loaded Shotgun data.
 
         :returns: The sg data dictionary for the associated item,
                   None if not available.
@@ -167,15 +210,3 @@ class PipelineConfigModel(ShotgunModel):
                 data.append(item.get_sg_data())
 
         return data
-
-    def get_hash(self):
-        """
-        Returns a hash representing the state of all sw entities
-        or None if nothing is loaded
-        """
-        sg_data = self.get_sg_data()
-        if sg_data is None:
-            return None
-        else:
-            return hash(str(sg_data))
-
