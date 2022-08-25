9fedff9542913bbd3b49134c1fbd10d48b957d3d
Norberto Moreno <norbermv.dev@gmail.com>
2022-08-25 09:07:57 -0500

    Auto-updated documentation based on branch/tag v5.8.5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'fix-developer-docs',
+    VERSION: 'v5.8.5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
