519e801fa08804b380b8111d8a7a69bfccc6406e
Shotgun-Rundeck <me.shotgun.rundeck.admin@autodesk.com>
2024-02-06 14:32:08 +0000

    Auto-updated documentation based on tag v5.8.8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v5.8.7',
+    VERSION: 'v5.8.8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
