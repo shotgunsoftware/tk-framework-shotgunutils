7bf28be74554331a223caf1bbec3aefb483e4bd5
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2025-06-09 17:19:17 -0500

    Replace `imp`

--- python/external_config/scripts/external_runner.py
+++ python/external_config/scripts/external_runner.py
@@ -13,16 +13,8 @@ import re
 import sys
 import errno
 import inspect
+import importlib.util
 import traceback
-
-# Until we remove the use of imp from this code,
-# we must suppress the warning here as it will pop up in the Shotgun browser
-# integration as a message box, when running in Python 3.4 >.
-import warnings
-
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    import imp
 
 
 # handle imports
@@ -251,12 +243,13 @@ def _import_py_file(python_path, name):
     :param str name: name of py file (without extension)
     :returns: Python object
     """
-    mfile, pathname, description = imp.find_module(name, [python_path])
-    try:
-        module = imp.load_module(name, mfile, pathname, description)
-    finally:
-        if mfile:
-            mfile.close()
+    module_path = os.path.join(python_path, f"{name}.py")
+    spec = importlib.util.spec_from_file_location(name, module_path)
+    if spec is None or spec.loader is None:
+        raise ImportError(f"Could not find module {name} at {module_path}")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
     return module
 
 
